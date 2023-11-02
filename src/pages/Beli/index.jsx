1217b3ec8b177ba4472847c743e4c695b2bc0410
iqoypresto <rfqimaulana888@gmail.com>
2023-11-02 19:48:06 +0800

    edit beli page

--- src/pages/Beli/index.jsx
+++ src/pages/Beli/index.jsx
@@ -1,20 +1,56 @@
-import React from "react";
-import Navbar from "../../components";
+import React, { useEffect, useState } from "react";
+import { Navbar, Filter, FilterModal, PropertyCard } from "../../components";
 
 export const Beli = () => {
+    const [propertiData, setPropertiData] = useState([])
+    const [openModal, setOpenModal] = useState(false)
+    const getData = async () => {
+        try {
+            const res = await fetch('https://sheet.best/api/sheets/9b36cbc6-cb51-4c3d-b3dc-e9891a644f88')
+            setPropertiData(await res.json())
+            console.log(propertiData)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
 
     return (
         <>
+            {openModal &&
+                <FilterModal closeModal={setOpenModal} />
+            }
             <Navbar />
-            <div className="content">
-                <div className="max-w-screen-xl mx-auto">
-                    <div>Home {'>'} Beli</div>
-                    <div>
-                        <form className="flex" action="">
-                            <input className="border h-[30px] p-5 outline-none rounded-[10px]" type="text" placeholder="Cari" />
-                            <button>a</button>
-                        </form>
-                    </div>
+            <div className="max-w-screen-xl mx-auto text-xs mt-[10px]">
+                Home {'>'} Beli
+            </div>
+            <Filter closeModal={setOpenModal} />
+            <div className="max-w-screen-xl mx-auto my-[20px]">
+                <div className="mb-[15px]">
+                    <h5>{propertiData.length} Properti Dijual di Indonesia</h5>
+                    <h5>Ditemukan <a href="" className="hover:text-red-300 text-red-500">Buat Pengingat</a></h5>
+                </div>
+                <div className="space-y-[15px]">
+                    {
+                        propertiData.length > 0 &&
+                        propertiData.map((properti) => (
+                            <PropertyCard 
+                            name       ={properti.name} 
+                            address    ={properti.address} 
+                            tower      ={properti.tower} 
+                            type       ={properti.property_type.replace(/^./, properti.property_type[0].toUpperCase())}
+                            bedroom    ={properti.bedroom} 
+                            bathroom   ={properti.bathroom} 
+                            size       ={parseInt(properti.size)} 
+                            furnished  ={properti.furnished.replace(/^./, properti.furnished[0].toUpperCase())}
+                            price      ={properti.price.replace(/(\d{3})(\d{3})(\d{3})/, '$1,$2,$3')} 
+                            img        ={properti.galleries}
+                            />
+                        ))
+                    }
                 </div>
             </div>
         </>
