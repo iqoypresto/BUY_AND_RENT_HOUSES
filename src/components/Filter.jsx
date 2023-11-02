2e5fd1c770bac2b78efbe8c699f3ecaed5a49395
iqoypresto <rfqimaulana888@gmail.com>
2023-11-02 23:39:36 +0800

    fix some issues

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -11,7 +11,7 @@ export const Filter = ({closeModal, searchFilter}) => {
             <div className="w-full shadow-md py-[10px] sticky top-[64px] bg-white">
                 <form className="max-w-screen-xl mx-auto flex gap-[15px]" action="" onSubmit={handleSubmit}>
                     <div className="w-full flex items-center border border-gray-400 rounded-[8px]  py-2 px-4 overflow-hidden">
-                        <input list="lokasi" type="text" className="w-full pe-[10px] outline-none appearance-none" placeholder="Cari Lokasi" onChange={(e) => searchFilter(e.target.value)} />
+                        <input list="lokasi" type="text" className="w-full pe-[10px] outline-none appearance-none" placeholder="Cari Lokasi" onChange={(e) => searchFilter(e.target.value.toLowerCase())} />
                         <IoSearchOutline size='20px' color="gray" />
                     </div>
                     <button className="border border-gray-400 py-2 px-4 rounded-[8px] flex items-center gap-[5px]" onClick={() => {
